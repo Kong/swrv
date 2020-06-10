65770c67f09438cdfa068c7e7a078a6f76c90594
Darren Jennings <dmjenn02@gmail.com>
2020-06-09 21:35:55 -0400

    docs(example/vite) add vite vue3 example

---
+++ examples/vite/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <h1>Hello Vite + Vue 3!</h1>
+  <p>Edit ./App.vue to test hot module replacement (HMR).</p>
+  <HelloWorld :count="count" />
+  <p>
+    <span>Count is: {{ count }}</span>
+  </p>
+  <button @click="count++">increment</button>
+  <button @click="count--">decrement</button>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  components: { HelloWorld },
+  data: () => ({ count: 1 })
+}
+</script>
+
+<style scoped>
+h1 {
+  color: #4fc08d;
+}
+
+h1, p {
+  font-family: Arial, Helvetica, sans-serif;
+}
+</style>
