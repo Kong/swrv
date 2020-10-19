8fe87dc73df819283940ccb7872e8555b5a19595
Darren Jennings <darren@konghq.com>
2020-10-19 00:19:48 -0400

    feat(mutate) force revalidate on mutate (#97)
    mutate should be immune to deduping interval
    fix mutate type
    
    Fixes #87

--- src/use-swrv.ts
+++ src/use-swrv.ts
@@ -233,14 +233,16 @@ function useSWRV<Data = any, Error = any> (...args): IResponse<Data, Error> {
     }
 
     const fetcher = data || fn
-    if (!fetcher || !isDocumentVisible()) {
+    if (!fetcher || !isDocumentVisible() || (opts?.forceRevalidate !== undefined && !opts?.forceRevalidate)) {
       stateRef.isValidating = false
       return
     }
 
     // Dedupe items that were created in the last interval #76
     if (cacheItem) {
-      const shouldRevalidate = (Date.now() - cacheItem.createdAt) >= config.dedupingInterval
+      const shouldRevalidate = Boolean(
+        ((Date.now() - cacheItem.createdAt) >= config.dedupingInterval) || opts?.forceRevalidate
+      )
 
       if (!shouldRevalidate) {
         stateRef.isValidating = false
@@ -388,10 +390,15 @@ function useSWRV<Data = any, Error = any> (...args): IResponse<Data, Error> {
     // do nothing
   }
 
-  return {
+  const res: IResponse = {
     ...toRefs(stateRef),
-    mutate: revalidate
-  } as IResponse<Data, Error>
+    mutate: (data, opts: revalidateOptions) => revalidate(data, {
+      ...opts,
+      forceRevalidate: true
+    })
+  }
+
+  return res
 }
 
 function isPromise<T> (p: any): p is Promise<T> {
