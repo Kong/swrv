df12396b97ef05019c7d359d76a21a005d2d280e
Darren Jennings <darren@konghq.com>
2020-12-20 12:28:39 -0500

    fix(compat) use .proxy for newest composition-api (#121)

--- src/use-swrv.ts
+++ src/use-swrv.ts
@@ -156,7 +156,7 @@ function useSWRV<Data = any, Error = any> (...args): IResponse<Data, Error> {
   let isHydrated = false
 
   const instance = getCurrentInstance() as any
-  const vm = instance
+  const vm = instance.proxy || instance // https://github.com/vuejs/composition-api/pull/520
   const IS_SERVER = vm.$isServer
 
   // #region ssr
