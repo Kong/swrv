a852fbb1379e00f23712c0c3fc513ed117ff81a0
Adam DeHaven <adam.dehaven@konghq.com>
2022-08-06 11:28:41 -0400

    chore(deps): upgrade dependencies

--- tests/test-compat-all.sh
+++ tests/test-compat-all.sh
@@ -2,5 +2,7 @@
 
 set -e
 
-tests/test-compat.sh "3.1.5"
-tests/test-compat.sh "3.2.31"
+tests/test-compat.sh "2.7.0"
+tests/test-compat.sh "2.7.8"
+# tests/test-compat.sh "3.1.5"
+# tests/test-compat.sh "3.2.31"
--- tests/test-compat.sh
+++ tests/test-compat.sh
@@ -4,5 +4,6 @@ set -e
 
 echo "running unit tests with 'vue@$1'"
 yarn add -W -D vue@$1
+yarn add -W -D vue-template-compiler@$1
 # yarn build:esm # needed for ssr
 yarn test
