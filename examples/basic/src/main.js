0ab366e14dce6500661853d7fce6d974f7b95477
Adam DeHaven <adam.dehaven@konghq.com>
2022-08-06 13:36:24 -0400

    fix(examples): update basic example structure

--- examples/basic/src/main.js
+++ examples/basic/src/main.js
@@ -1,4 +1,7 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+/* eslint-disable */
+import Vue from 'vue'
+import App from './App'
 
-createApp(App).mount('#app')
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
