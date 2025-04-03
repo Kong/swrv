1e1f352e7df1d1d46ca793ceda957083b4a2cfa8
chenzhaozheng <389428742@qq.com>
2025-04-03 16:33:48 +0800

    feat: add indexedDB cache

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,6 @@ module.exports = {
   preset: '@vue/cli-plugin-unit-jest/presets/typescript-and-babel',
   testMatch: [
     '<rootDir>/tests/**/*.spec.[jt]s?(x)'
-  ]
+  ],
+  setupFiles: ['fake-indexeddb/auto']
 }
